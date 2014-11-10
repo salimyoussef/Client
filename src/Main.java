fa11179b182a4ebaf4d2a742cfb046b10ea9f636
Youssef Salim <salim.youssef@etu.unice.fr>
2014-11-10 19:43:46 +0100

    Analyse de commande

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 import client.Client;
 import exceptions.ConnectionNotOpenedException;
 
@@ -5,11 +7,25 @@ import exceptions.ConnectionNotOpenedException;
 public class Main {
 	public static void main(String[] args) {
 		Client client = new Client();
-		client.open();
+		Scanner s = new Scanner(System.in);
+		String req;
+		
 		
 		
 		try {
-			String rep = client.send("bonjour !");
+			while(true){
+				req = s.next();
+				if(req.equals("ajouter")){
+					String nom =s.next();
+					String surnom = s.next();
+					req = req + " " + nom + " " + surnom;
+					client.setServer("10.212.102.245", 5000);
+					client.open();
+					String resp = client.send(req);
+					System.out.println(resp);
+					client.close();
+				}
+			}
 		} catch (ConnectionNotOpenedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -17,7 +33,7 @@ public class Main {
 		
 		
 		
-		client.close();
+		
 		
 		
 	}
--- src/client/Client.java
+++ src/client/Client.java
@@ -21,6 +21,11 @@ public class Client {
 		connectionOpened = false;
 	}
 	
+	public void setServer(String host, int port){
+		serverName = host;
+		serverPort = port;
+	}
+	
 	public void open(){
 		try {
 			socket = new Socket(serverName, serverPort);
@@ -57,9 +62,6 @@ public class Client {
 	public void close(){
 		sendMessage("bye");
 		connectionOpened = false;
-		
-		
-		
 	}
 	
 	private void sendMessage(String message){
