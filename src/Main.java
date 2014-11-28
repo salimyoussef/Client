4e6b0ce40e84326b05afda938857b87bb4492c19
Youssef Salim <salim.youssef@etu.unice.fr>
2014-11-28 08:38:31 +0100

    kkkk

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,51 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 
 import client.Client;
+import client.ProtocolClient;
 import exceptions.ConnectionNotOpenedException;
-
 
 public class Main {
 	public static void main(String[] args) {
 		Client client = new Client();
 		Scanner s = new Scanner(System.in);
 		String req;
-		
-		
-		
-		try {
-			while(true){
+		String resp = "e";
+		ProtocolClient pc = new ProtocolClient();
+		LinkedList<String> parametres = new LinkedList<>();
+
+		do {
+			try {
 				req = s.next();
-				if(req.equals("ajouter")){
-					String nom =s.next();
+				if (req.equals("ajouter")) {
+					String nom = s.next();
 					String surnom = s.next();
-					req = req + " " + nom + " " + surnom;
-					client.setServer("10.212.102.245", 5000);
+					parametres.add(nom);
+					parametres.add(surnom);
+					req = pc.message(req, parametres);
+					// client.setServer("10.212.102.245", 5000);
 					client.open();
-					String resp = client.send(req);
+					resp = client.send(req);
 					System.out.println(resp);
 					client.close();
 				}
+				if (req.equals("enregistrer")) {
+					String nom = s.next();
+					String surnom = s.next();
+					parametres.add(nom);
+					parametres.add(surnom);
+					req = pc.message(req, parametres);
+					// client.setServer("10.212.102.245", 5000);
+					client.open();
+					resp = client.send(req);
+					System.out.println(resp);
+					client.close();
+				}
+			} catch (ConnectionNotOpenedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		} catch (ConnectionNotOpenedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		
-		
-		
-		
+		} while (!resp.equals("bye"));
+
 	}
 }
---
+++ src/client/ProtocolClient.java
@@ -0,0 +1,21 @@
+package client;
+
+import java.util.LinkedList;
+
+public class ProtocolClient {
+	
+	
+	public String message(String type, LinkedList<String> parametres){
+		String message;
+		message = "{\"type\" : \""+type+"\", \"parametres\" : [";
+		int i = 0;
+		for(String param : parametres){
+			if(i != 0) 
+				message = message + ",";
+			message = message + "\""+param+"\"";
+			i++;
+		}
+		message = message + "]}";
+		return message;
+	}
+}
